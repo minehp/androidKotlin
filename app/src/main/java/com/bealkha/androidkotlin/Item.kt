46915f60238a27f5969e6d50e8825b382920477a
Yuda Ferry Mahendra <juncyboy@gmail.com>
2018-10-25 16:09:59 +0700

    try anko layout and commons but failed

--- app/src/main/java/com/bealkha/androidkotlin/Item.kt
+++
@@ -1,3 +0,0 @@
-package com.bealkha.androidkotlin
-
-data class Item (val name: String?, val image: Int?)
--- app/src/main/java/com/bealkha/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/bealkha/androidkotlin/MainActivity.kt
@@ -1,62 +1,102 @@
 package com.bealkha.androidkotlin
 
-import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.util.Log
-import android.view.View
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import com.bealkha.androidkotlin.R.color.colorAccent
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
-class MainActivity : Activity() {
-    private var items: MutableList<Item> = mutableListOf()
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*setContentView(R.layout.activity_main)*/
-
-        /*val list = findViewById<RecyclerView>(R.id.club_list)
-        initData()
-        list.layoutManager = LinearLayoutManager(this)
-        list.adapter = RecyclerViewAdapter(this, items) {
-            Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT).show()
-        }*/
-
-        /*initData()
-        club_list.layoutManager = LinearLayoutManager(this)
-        club_list.adapter = RecyclerViewAdapter(this, items) {
-            Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT).show()
-        }*/
-        ankoUITrial().setContentView(this)
+        MainActivityUI().setContentView(this)
     }
 
-    private fun initData() {
-        val name = resources.getStringArray(R.array.club_name)
-        val image = resources.obtainTypedArray(R.array.club_image)
-        items.clear()
-        for( i in name.indices) {
-            items.add(Item(name[i], image.getResourceId(i,0)))
-        }
-        image.recycle()
-    }
 
-    class ankoUITrial: AnkoComponent<MainActivity>, AnkoLogger {
+    class MainActivityUI : AnkoComponent<MainActivity> {
         override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
-            verticalLayout {
-                imageView(R.drawable.img_arsenal).lparams(width= matchParent) {
-                    padding = dip(20)
-                    margin = dip(15)
+            verticalLayout{
+                padding = dip(16)
+
+                val name = editText {
+                    hint = "What's your name?"
                 }
 
-                val name = editText() {
-                    hint = "What's your name ?"
+                button("Say Hello"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+
+
+                    onClick { toast("Hello, ${name.text}!") }
+
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
                 }
 
-                val btn = button("Say Hello")
-                btn.setOnClickListener {
-                    info("Halloooooooooooo ====================")
+                button("Show Alert"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+
+                    onClick {
+                        alert("Happy Coding!", "Hello, ${name.text}!") {
+                            yesButton { toast("Oh…") }
+                            noButton {}
+                        }.show()
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Selector"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+
+                    onClick {
+                        val club = listOf("Barcelona", "Real Madrid", "Bayern Munchen", "Liverpool")
+                        selector("Hello, ${name.text}! What's football club do you love?", club) { _, i ->
+                            toast("So you love ${club[i]}, right?")
+                        }
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Snackbar"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+
+                    onClick {
+                        snackbar(name, "Hello, ${name.text}!")
+
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Progress Bar"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+
+                    onClick {
+                        indeterminateProgressDialog("Hello, ${name.text}! Please wait...").show()
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Go to Second Activity"){
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+
+                    onClick {
+                        startActivity<SecondActivity>("name" to "${name.text}")
+                    }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
                 }
             }
         }
